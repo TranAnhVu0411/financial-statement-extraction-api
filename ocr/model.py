08ccc823e18b17e041942e3e4e07cbea3fcd6687
VuTranAnh <vu.ta183674@sis.hust.edu.vn>
2023-07-23 20:23:36 +0700

    Update model.py

--- ocr/model.py
+++ ocr/model.py
@@ -1,8 +1,9 @@
 from ocr.path import SIGNATURE_LOGO_DETECTION_PATH, TABLE_DETECTION_PATH, TABLE_STRUCTURE_RECOGNITION_PATH
-from ocr.utils.load_models import YOLOv5_model, Signver_clean_model, VietOCR_model, CRAFT_model
+# from ocr.utils.load_models import Signver_clean_model
+from ocr.utils.load_models import YOLOv5_model, VietOCR_model, CRAFT_model
 
 signature_logo_detection = YOLOv5_model(SIGNATURE_LOGO_DETECTION_PATH)
-cleaner = Signver_clean_model()
+# cleaner = Signver_clean_model()
 table_detection = YOLOv5_model(TABLE_DETECTION_PATH, threshold=True)
 detector = VietOCR_model()
 net = CRAFT_model()
